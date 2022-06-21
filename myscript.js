5d59875a7c6df0ee47f0bd0266d42c27df17a032
liam-rng <liam.scott.7798@gmail.com>
2022-06-21 13:49:46 -0400

    Add buttons, a forEach loop, and play a round when a button is clicked
    These implementations changes the game to function with buttons instead
    of using the console.

--- myscript.js
+++ myscript.js
@@ -4,42 +4,53 @@ function computerPlay() {
 }
 
 function promptPlayer() {
-    var choice = window.prompt("Howdy!Are you down for a friendly game of Rock, Paper, Scissors? \nType in your choice:")
+    var choice = window.prompt("Howdy! Are you down for a friendly game of Rock, Paper, Scissors? \nType in your choice:")
     let str1 = choice.toString();
     return str1.toUpperCase();
 }
-
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection == playerSelection)
-        return "A tie... Was that your best?";
-    else if (computerSelection == "PAPER" && playerSelection == "ROCK" || computerSelection == "ROCK" && playerSelection == "SCISSORS" || computerSelection == "SCISSORS" && playerSelection == "PAPER")
-        return "Haha! You lost.";
-    else if ((playerSelection == "PAPER" && computerSelection == "ROCK" || playerSelection == "ROCK" && computerSelection == "SCISSORS" || playerSelection == "SCISSORS" && computerSelection == "PAPER"))
-        return "You think you're smart huh?!";
-
-    else
-        return "You only have three choices genius, it really isn't that hard.";
-
-}
-
-
-function game(games = 5) {
-    var pc_wins = 0, player_wins = 0, ties = 0;
-    for (let i = 0; i < games; i++) {
-
-        let pc_choice = computerPlay();
-        let player_choice = promptPlayer();
-
-        console.log(`The computer played ${pc_choice} and you played ${player_choice}.`)
-
-        let str1 = playRound(player_choice, pc_choice);
-
-        if (str1 == "A tie... Was that your best?")
-            ties++;
-        else if (str1 == "Haha! You lost.")
-            pc_wins++;
-        else if (str1 == "You think you're smart huh?!")
-            player_wins++;
+let ties = 0, wins = 0, losses = 0;
+function playRound(human_sel, pc_sel) {
+    if (pc_sel == human_sel){
+        ties++;
+        return (`You both selected ${pc_sel}. A tie, well done.`);
     }
-    return (pc_wins > player_wins ? `Wow ${pc_wins}x${player_wins}. The computer won, of course.` : pc_wins === player_wins ? `A tie: ${pc_wins}x${player_wins}. You did better than expected.` : `Y-You won?! ${pc_wins}x${player_wins}. No one will believe you...`)
+    else if (pc_sel == "PAPER" && human_sel == "ROCK" || pc_sel == "ROCK" && human_sel == "SCISSORS" || pc_sel == "SCISSORS" && human_sel == "PAPER"){
+        wins++; 
+        return (`You chose ${human_sel} and the computer chose ${pc_sel}. Haha! Gotcha!`);
 }
+    else {
+        losses++;
+        return (`You chose ${human_sel} and the computer chose ${pc_sel}. What a tryhard.`);
+    }
+}
+
+// function game(games = 5) {
+//     var pc_wins = 0, player_wins = 0, ties = 0;
+//     for (let i = 0; i < games; i++) {
+
+//         let pc_choice = computerPlay();
+//         let player_choice = promptPlayer();
+
+//         console.log(`The computer played ${pc_choice} and you played ${player_choice}.`)
+
+//         let str1 = playRound(player_choice, pc_choice);
+
+//         if (str1 == "A tie, well done.")
+//             ties++;
+//         else if (str1 == "Haha! Gotcha!")
+//             pc_wins++;
+//         else if (str1 == "What a tryhard.")
+//             player_wins++;
+//     }
+//     return (pc_wins > player_wins ? `Wow ${pc_wins}x${player_wins}. The computer won, of course.` : pc_wins === player_wins ? `A tie: ${pc_wins}x${player_wins}. You did better than expected.` : `Y-You won?! ${pc_wins}x${player_wins}. No one will believe you...`)
+// }
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let player_choice = button.id.toUpperCase();
+        let pc_choice = computerPlay();
+        console.log(playRound(player_choice, pc_choice))
+        console.log(wins,ties,losses);
+    });
+  });
